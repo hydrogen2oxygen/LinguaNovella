7ff87dfa822fb40e1fe91b427b6a0986c37b2ac1
hydrogen2oxygen <luxuspeter@googlemail.com>
2024-06-17 10:14:46 +0200

    Virtual Keyboard works, but still WIP

--- ui/src/app/components/read/read.component.ts
+++ ui/src/app/components/read/read.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {LinguaService} from "../../services/lingua.service";
 import {Word} from "../../domains/word";
 
+import Keyboard from "simple-keyboard";
+
 @Component({
   selector: 'app-read',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.scss']
 })
@@ -16,11 +19,52 @@ export class ReadComponent implements OnInit {
   from_lang:string = "ru"
   to_lang:string = "en"
 
+  value = "";
+  keyboard: Keyboard|undefined;
+
   constructor(private lingua:LinguaService) { }
 
   ngOnInit(): void {
     this.text.setValue("Это о том, как быстро выучить новые слова")
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button),
+      theme: "hg-theme-default darkKeyboardTheme"
+    });
   }
+
+  onChange = (input: string) => {
+    this.value = input;
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+
+    const buttonElement = document.querySelector(`.hg-button-${button}`);
+    if (buttonElement) {
+      buttonElement.classList.add('animated');
+      setTimeout(() => buttonElement.classList.remove('animated'), 300); // Duration of the animation
+    }
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard?.setInput(event.target.value);
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard?.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard?.setOptions({
+      layoutName: shiftToggle
+    });
+  };
 
   readText() {
     // @ts-ignore
